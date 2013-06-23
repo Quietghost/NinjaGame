ed0eec020087252ecc4ae372f1a9bf28e0c90142
sseypt <st6v6@web.de>
2013-06-23 23:34:59 +0200

    get in touch with tmx loader and classes

--- ninja-game-prototype/src/com/me/ninja_game_prototype/helper/LevelLoader.java
+++ ninja-game-prototype/src/com/me/ninja_game_prototype/helper/LevelLoader.java
@@ -1,9 +1,19 @@
 package com.me.ninja_game_prototype.helper;
 
-import com.badlogic.gdx.math.Vector2;
-import com.me.ninja_game_prototype.model.ExitModel;
-import com.me.ninja_game_prototype.model.NinjaModel;
-import com.me.ninja_game_prototype.model.ObstacleModel;
+import java.util.Iterator;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapLayers;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.MapProperties;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTile;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.me.ninja_game_prototype.model.WorldModel;
 
 public class LevelLoader {
@@ -28,17 +38,88 @@ public class LevelLoader {
 		WorldModel.get().clear();
 		
 		// TODO load tmx file
-//        TiledMap map;
-//        AtlasTmxMapLoader atlas;
-//        
-//        // Load the tmx file into map
-//        map = new TmxMapLoader().load("map2.tmx");
+		try {
+//			File fXmlFile = Gdx.files.internal("map2.tmx").file();
+//			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+//			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+//			Document doc = dBuilder.parse(fXmlFile);
+			
+			// Load the tmx file into map
+	        TiledMap map = new TmxMapLoader().load("map2.tmx");
+	        Iterator it;
+	        
+	        it = map.getProperties().getKeys();
+	        while (it.hasNext()) {
+	        	String key = (String) it.next();
+	        	System.out.println(key+": "+map.getProperties().get(key));
+			}	        
+	        
+	        MapLayers layers = map.getLayers();
+	        System.out.println("Layers: "+layers.getCount());
+	        TiledMapTileLayer floor = (TiledMapTileLayer) layers.get("L_Floor");
+	        TiledMapTileLayer wall = (TiledMapTileLayer) layers.get("L_Wall");
+	        
+	        it = layers.iterator();
+	        while (it.hasNext()) {
+	        	MapLayer l = (MapLayer) it.next();
+	        	System.out.println("Layer: "+l.getName());
+			}
+	        
+	        MapObjects floorO = floor.getObjects();
+	        System.out.println("Objects: "+floorO.getCount());
+	        MapProperties floorP = floor.getProperties();
+	        
+	        it = floorO.iterator();
+	        while (it.hasNext()) {
+	        	MapObject o = (MapObject) it.next();
+	        	System.out.println("O: "+o.getName());
+			}
+	        
+	        it = floorP.getKeys();
+	        while (it.hasNext()) {
+	        	String key = (String) it.next();
+	        	System.out.println("P "+key+": "+floorP.get(key));
+			}
+	        it = floorP.getValues();
+	        while (it.hasNext()) {
+	        	String value = (String) it.next();
+	        	System.out.println("P "+value);
+			}
+	        
+	        System.out.println("Layer: "+floor.getHeight()+" "+floor.getWidth()+" - "+floor.getTileHeight()+" "+floor.getTileWidth());
+	        Cell c = floor.getCell(1, 1);
+	        System.out.println("Cell: "+c.toString());
+	        TiledMapTile t = c.getTile();
+	        
+	        it = t.getProperties().getKeys();
+	        while (it.hasNext()) {
+	        	String key = (String) it.next();
+	        	System.out.println("Tile "+key+": "+t.getProperties().get(key));
+			}
+	        
+	        TextureRegion tr = t.getTextureRegion();
+	        System.out.println("TextureRegion: "+tr.getRegionHeight()+" "+tr.getRegionWidth()+" - "+tr.getRegionY()+" "+tr.getRegionX());
+	        
+	        Texture tx = tr.getTexture();
+	        System.out.println("Texture: "+tx.toString());
+			
+//		} catch (ParserConfigurationException e) {
+//			e.printStackTrace();
+//		} catch (SAXException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
-		WorldModel.get().addObstacles(new ObstacleModel(new Vector2(320,310), 64, 64));
-		WorldModel.get().addObstacles(new ObstacleModel(new Vector2(100,110), 108, 92));
+		System.exit(0);
 		
-		WorldModel.get().setNinja(new NinjaModel(60, 54, 62, new Vector2(200,400)));
-		
-		WorldModel.get().setExit(new ExitModel(new Vector2(600,110), 54, 100));
+//		WorldModel.get().addObstacles(new ObstacleModel(new Vector2(320,310), 64, 64), "");
+//		WorldModel.get().addObstacles(new ObstacleModel(new Vector2(100,110), 108, 92), "");
+//		
+//		WorldModel.get().setNinja(new NinjaModel(60, 54, 62, new Vector2(200,400)), "", "");
+//		
+//		WorldModel.get().setExit(new ExitModel(new Vector2(600,110), 54, 100), "");
 	}
 }
