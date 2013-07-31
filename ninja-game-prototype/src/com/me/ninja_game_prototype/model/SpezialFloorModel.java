0afae2c6199eb532e0f3a8c7ac5b93a047dc2eed
sseypt <st6v6@web.de>
2013-07-31 18:38:43 +0200

    steve hates egit or may not understand how to use it (unlikely)

---
+++ ninja-game-prototype/src/com/me/ninja_game_prototype/model/SpezialFloorModel.java
@@ -0,0 +1,28 @@
+package com.me.ninja_game_prototype.model;
+
+import com.badlogic.gdx.math.Polygon;
+
+public class SpezialFloorModel
+{
+	public static final String TYPE_NONE = "none";
+	public static final String TYPE_STONE = "Stone";
+	
+	private String type;
+	
+	public SpezialFloorModel(String name,Polygon area)
+	{
+		String[] tmp = name.split("_");
+		
+		switch (tmp[0]) {
+		case TYPE_STONE:
+			type = TYPE_STONE;
+			break;
+		default:
+			type = TYPE_NONE;
+		}
+	}
+
+	public String getType() {
+		return type;
+	}
+}
